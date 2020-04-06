25933bf3a64ad826ad23e71ef6d3c1d8c786f658
Vishu1910 <63246619+Vishu1910@users.noreply.github.com>
2020-04-06 21:21:55 +0530

    Add files via upload

---
+++ positivenum.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list=[]
+a=[]
+x=int(input())
+for num in range(x):
+    num=int(input())
+    list.append(num)
+for num in range(x):
+    if list[num]>0:
+        a.append(list[num])
+print(a)
+        
+
+
+# 
+# 
+
+# In[ ]:
+
+
+
+
